30ca83d4e452f4e6f04cee93450b656c1cd3d505
Max Cobb <5754073+maxxfrazer@users.noreply.github.com>
2023-10-29 19:18:58 +0000

    added visionOS support

--- Package-5.7.swift
+++ Package-5.7.swift
@@ -1,11 +1,11 @@
-// swift-tools-version:5.9
+// swift-tools-version:5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "RealityGeometries",
-    platforms: [.iOS(.v15), .macOS(.v12), .macCatalyst(.v15), .visionOS(.v1), .tvOS(.v17)],
+    platforms: [.iOS(.v15), .macOS(.v12), .macCatalyst(.v15)],
     products: [.library(name: "RealityGeometries", targets: ["RealityGeometries"])],
     dependencies: [],
     targets: [
