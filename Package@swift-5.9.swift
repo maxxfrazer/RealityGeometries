6e3ce738df779a38f55c2cd532d6103844790636
Max Cobb <maxxc@mac.com>
2023-11-23 14:00:42 +0000

    moved special Package.swift to the 5.9 version, and added spi yml

--- Package@swift-5.9.swift
+++ Package@swift-5.9.swift
@@ -1,11 +1,11 @@
-// swift-tools-version:5.7
+// swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "RealityGeometries",
-    platforms: [.iOS(.v15), .macOS(.v12), .macCatalyst(.v15)],
+    platforms: [.iOS(.v15), .macOS(.v12), .macCatalyst(.v15), .visionOS(.v1)],
     products: [.library(name: "RealityGeometries", targets: ["RealityGeometries"])],
     dependencies: [],
     targets: [
